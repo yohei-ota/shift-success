e41df7a4dd2317829f945a98c16f8ee1f8d2b3da
yohei-ota <freeeedom20xx@gmail.com>
2021-08-27 18:57:17 +0900

    管理ユーザーとメンバーユーザーのモデルとルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    password: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    password: 'users/passwords',
+    registrations: 'users/registrations'
+  }
 end
