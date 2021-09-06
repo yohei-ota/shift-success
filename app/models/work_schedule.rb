79b89943f6d3817a504c9ee1a01157b254666782
yohei-ota <freeeedom20xx@gmail.com>
2021-09-06 14:50:42 +0900

    モデル単体テストコードの実装とバリデーションの追加

--- app/models/work_schedule.rb
+++ app/models/work_schedule.rb
@@ -2,9 +2,11 @@ class WorkSchedule < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
+  validates :holiday, inclusion: {in: [true, false]}
   validate :each_duplicate_check
   # validate :duplicate_check
   validate :time_check
+  validate :now_time_check
 
   # シフト希望があるなら開始と終了両方ないといけない
   def each_duplicate_check
@@ -16,6 +18,14 @@ class WorkSchedule < ApplicationRecord
   # end
   # 開始と終了時刻に矛盾があってはいけない
   def time_check
-    errors.add(:datetime_out, "は勤務開始日時より遅い日時を入力してください") if self.datetime_in > self.datetime_out
+    if self.datetime_in.present? || self.datetime_out.present?
+      errors.add(:datetime_out, "は勤務開始日時より遅い日時を入力してください") if self.datetime_in > self.datetime_out
+    end
+  end
+  # 過去の日時は受け付けない
+  def now_time_check
+    if self.datetime_in.present? || self.datetime_out.present?
+      errors.add(:datetime_in, "日時を正しく入力してください") if self.datetime_in < DateTime.now
+    end
   end
 end
--- spec/models/work_schedule_spec.rb
+++ spec/models/work_schedule_spec.rb
@@ -1,5 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe WorkSchedule, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @work_schedule = FactoryBot.build(:work_schedule)
+  end
+
+  describe "シフト申請希望" do
+    context "シフト申請できるとき" do
+      it "必要項目が存在すれば申請できる" do
+        expect(@work_schedule).to be_valid
+      end
+      it "datetime_in,datetime_outがなくても申請できる" do
+        @work_schedule.datetime_in = ""
+        @work_schedule.datetime_out = ""
+        expect(@work_schedule).to be_valid
+      end
+      it "add_requestがなくても申請できる" do
+        @work_schedule.add_request = ""
+        expect(@work_schedule).to be_valid
+      end
+      it "datetime_in < datetime_outなら申請できる" do
+        @work_schedule.datetime_in = DateTime.now + 1
+        @work_schedule.datetime_out = DateTime.now + 2
+        expect(@work_schedule).to be_valid
+      end
+      it "datetime_inがなくても申請できる" do
+        @work_schedule.datetime_in = ""
+        @work_schedule.datetime_out = ""
+        expect(@work_schedule).to be_valid
+      end
+      it "add_requestがなくても申請できる" do
+        @work_schedule.add_request = ""
+        expect(@work_schedule).to be_valid
+      end
+    end
+
+    context "シフト希望申請できないとき" do
+      it "holidayが空では申請できない" do
+        @work_schedule.holiday = ""
+        @work_schedule.valid?
+        expect(@work_schedule.errors.full_messages).to include("Holiday is not included in the list")
+      end
+      it "datetime_in > datetime_outだと申請できない" do
+        @work_schedule.datetime_in = DateTime.now + 2
+        @work_schedule.datetime_out = DateTime.now + 1
+        @work_schedule.valid?
+        expect(@work_schedule.errors.full_messages).to include("Datetime out は勤務開始日時より遅い日時を入力してください")
+      end
+      # it "holidayがtrueの時datetime_inがあると申請できない" do
+      #   @work_schedule.holiday = true
+      #   @work_schedule.valid?
+      #   expect(@work_schedule.errors.full_messages).to include("hoge")
+      # end
+      # it "holidayがtrueの時datetime_outがあると申請できない" do
+      #   @work_schedule.holiday = true
+      #   @work_schedule.valid?
+      #   expect(@work_schedule.errors.full_messages).to include("hoge")
+      # end
+    end
+  end
 end
