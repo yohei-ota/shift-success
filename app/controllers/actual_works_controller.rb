d3b5da2ac77315a401b9362a8dbdd5bba3b76ea6
yohei-ota <freeeedom20xx@gmail.com>
2021-09-06 18:08:08 +0900

    シフト作成ページのHTML記述

--- app/controllers/actual_works_controller.rb
+++ app/controllers/actual_works_controller.rb
@@ -1,10 +1,11 @@
 class ActualWorksController < ApplicationController
 
   def new
-
+    @schedules = WorkSchedule.where(group_id: current_admin.group_id).where("datetime_in >= ?", Date.today)
+    @users = User.where(group_id: current_admin.group_id)
   end
 
   def create
-    
+
   end
 end
