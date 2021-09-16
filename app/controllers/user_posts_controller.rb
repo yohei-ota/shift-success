1f4a8e7a358bb344eec9c674f880a15e8e32a845
yohei-ota <freeeedom20xx@gmail.com>
2021-09-16 15:12:42 +0900

    メンバーユーザーのトップページのHTML,CSSの調整

--- app/controllers/user_posts_controller.rb
+++ app/controllers/user_posts_controller.rb
@@ -3,6 +3,6 @@ class UserPostsController < ApplicationController
   
   def index
     @today = Date.today
-    @works = ActualWork.where(group_id: current_user.group_id).where(user_id: current_user.id).where("date >= ?", Date.today).order("date ASC")
+    @works = ActualWork.where(group_id: current_user.group_id).where(user_id: current_user.id).where("date >= ?", Date.today.month).order("date ASC")
   end
 end
