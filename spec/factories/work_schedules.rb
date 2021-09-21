9e7a620cb17ddcb76dbf011c8b4d2667f7386bba
yohei-ota <freeeedom20xx@gmail.com>
2021-09-21 16:36:22 +0900

    保存される日付を本日から7日→再来週の月曜から7日に変更

--- spec/factories/work_schedules.rb
+++ spec/factories/work_schedules.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :work_schedule do
-    datetime_in  {DateTime.now + 1}
-    datetime_out {DateTime.now + 2}
+    datetime_in  {Date.today.days_since(14).beginning_of_week.day}
+    datetime_out {Date.today.days_since(14).beginning_of_week.day}
     holiday      {false}
     add_request  {Faker::Lorem.sentence}
     association :user
